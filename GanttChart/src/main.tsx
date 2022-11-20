46bb85fbaf4fef16ecbb3473748e02b02d58cf0f
DmitrySaf <dmitry.study@yandex.ru>
2022-11-20 12:05:54 +0300

    initialized basic styles and added fonts

--- GanttChart/src/main.tsx
+++ GanttChart/src/main.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './assets/css/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
