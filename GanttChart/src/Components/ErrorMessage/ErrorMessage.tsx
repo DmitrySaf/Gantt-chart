fff2b1b8d6efb590a04299eed17ae46e34f54aa2
DmitrySaf <dmitry.study@yandex.ru>
2022-11-20 19:25:07 +0300

    added errorMessage component to pop on failed fetch

---
+++ GanttChart/src/Components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,17 @@
+import { useAppDispatch } from "../../hooks/typedHooks";
+import { fetchProject } from "../../store/slices/ProjectSlice";
+
+import './ErrorMessage.scss';
+
+function ErrorMessage() {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="error-message">
+      <h1 className="error-message__title">Something went wrong... try again later</h1>
+      <button className="error-message__button" onClick={() => dispatch(fetchProject())}>try again</button>
+    </div>
+  );
+}
+
+export default ErrorMessage;
