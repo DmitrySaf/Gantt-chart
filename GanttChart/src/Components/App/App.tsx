e110c4e39e4b7c27bce8fbce7aace1c9c8a84d62
DmitrySaf <dmitry.study@yandex.ru>
2022-11-20 19:25:39 +0300

    added name of the chart and components on loading statuses

--- GanttChart/src/Components/App/App.tsx
+++ GanttChart/src/Components/App/App.tsx
@@ -2,24 +2,25 @@ import { useEffect } from 'react';
 
 import { fetchProject } from "../../store/slices/ProjectSlice";
 import { useAppSelector, useAppDispatch } from "../../hooks/typedHooks";
+import Spinner from "../Spinner/Spinner";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 import './App.scss';
 
 function App() {
-  const { loadingStatus } = useAppSelector((state) => state);
+  const { loadingStatus, name, period } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchProject());
   }, []);
 
-  if (loadingStatus === 'loading') {
-
-  }
+  if (loadingStatus === 'loading') return <Spinner />;
+  if (loadingStatus === 'error') return <ErrorMessage />
 
   return (
     <>
-      <h1></h1>
+      <h1>{name} / {period}</h1>
     </>
   );
 }
