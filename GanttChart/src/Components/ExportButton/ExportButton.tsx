7485f66957fc28524aed1e8b2a36a5824173edb8
DmitrySaf <dmitry.study@yandex.ru>
2022-11-20 19:46:14 +0300

    added export button component

---
+++ GanttChart/src/Components/ExportButton/ExportButton.tsx
@@ -0,0 +1,12 @@
+import './ExportButton.scss';
+
+function ExportButton() {
+  return (
+    <div className="export-button">
+      <div className="export-button__icon"></div>
+      <div className="export-button__text">Export</div>
+    </div>
+  );
+}
+
+export default ExportButton;
